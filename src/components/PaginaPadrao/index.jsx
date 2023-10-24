05a93d31c10a7776c6f00728dd16a50f5af1d8d1
yapeansa <yago.pereira@estudante.ufjf.br>
2023-10-24 17:30:49 -0300

    Alterando Olá, mundo.

--- src/components/PaginaPadrao/index.jsx
+++ src/components/PaginaPadrao/index.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Banner from "../Banner"
 
-const PaginaPadrao = () => {
+const PaginaPadrao = ({ children }) => {
     return (
         <main>
             <Banner />
             <Outlet />
+            {children}
         </main>
     )
 }
--- src/paginas/post/index.jsx
+++ src/paginas/post/index.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import posts from '@/json/posts.json'
 import PostModelo from "@/components/PostModelo"
 import ReactMarkdown from 'react-markdown'
@@ -16,19 +16,16 @@ export default function Post() {
     }
 
     return (
-        <Routes>
-            <Route path="*" element={<PaginaPadrao />}>
-                <Route index element={
-                    <PostModelo
-                        fotoCapa={`/assets/posts/${post.id}/capa.png`}
-                        titulo={post.titulo}
-                    >
-                        <div className="post-markdown-container">
-                            <ReactMarkdown>{post.texto}</ReactMarkdown>
-                        </div>
-                    </PostModelo>
-                } />
-            </Route>
-        </Routes>
+        <PaginaPadrao>
+            <PostModelo
+                fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                titulo={post.titulo}
+            >
+                <div className="post-markdown-container">
+                    <ReactMarkdown>{post.texto}</ReactMarkdown>
+                </div>
+                <h1>Cards aqui</h1>
+            </PostModelo>
+        </PaginaPadrao>
     )
 }
